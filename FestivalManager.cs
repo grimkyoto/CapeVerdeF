9dcf5acce5fe94596f93a145c045bb53b6356e1c
grimkyoto <135552402+grimkyoto@users.noreply.github.com>
2025-05-01 21:06:53 -0400

    Update FestivalManager.cs

--- FestivalManager.cs
+++ FestivalManager.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
+using System.Text;
 
 namespace CapeVerdeCulturalFestival
 {
     public class FestivalManager
     {
+        public string ManagerName { get; set; } = "Aniyah Rodrigues";
         public string Name { get; } = "Cape Verdean-American Festival";
         public int Year { get; } = DateTime.Now.Year;
         public List<Participant> Participants { get; } = new List<Participant>();
@@ -25,7 +28,29 @@ namespace CapeVerdeCulturalFestival
             if (p.Category == "Cachupa Cooking" && string.IsNullOrWhiteSpace(p.Contact))
                 throw new ArgumentException("Chefs must provide contact info!");
         }
-
+ public void ExportToCSV(string filePath)
+    {
+        var csv = new StringBuilder();
+        
+        // header
+        csv.AppendLine($"Cape Verdean Festival,{Year},Managed by: {ManagerName}");
+        csv.AppendLine("Name,Category,Contact,Fee,Special Attributes");
+        
+       
+        foreach (var p in Participants)
+        {
+            string specialAttributes = p switch
+            {
+                MornaParticipant m => $"Instrument: {m.Instrument}, Traditional: {m.IsTraditional}",
+                FunanaDanceParticipant f => $"Dancers: {f.DancerCount}",
+                _ => "General Participant"
+            };
+            
+            csv.AppendLine($"\"{p.Name}\",{p.Category},{p.Contact},{p.FeePaid},\"{specialAttributes}\"");
+        }
+        
+        File.WriteAllText(filePath, csv.ToString(), Encoding.UTF8);
+    }
         public string GetCulturalStats()
         {
             return $@"Cultural Breakdown:
